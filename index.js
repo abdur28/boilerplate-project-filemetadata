a7a44c4487252a8add1a29a6d49ba089dd9b3287
abdur28 <abdurrahmanidris28@gmail.com>
2024-08-07 02:45:00 +0300

    inital update

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const multer = require('multer');
+const bodyParser = require("body-parser");
+const cors = require('cors');
 require('dotenv').config()
 
 var app = express();
 
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  res.json({ name: req.file.originalname,type: req.file.mimetype, size: req.file.size });
+});
 
 
 
